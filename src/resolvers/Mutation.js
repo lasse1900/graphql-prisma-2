887eeb332959af92add61aa93cf6e960f829c5a1
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-03-30 16:26:53 +0300

    creating auth tokens with JSON web tokens

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,19 +1,24 @@
 import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
 const Mutation = {
   async createUser(parent, args, { prisma }, info) {
     if (args.data.password.length < 8) {
-      throw new Error('Password must be 8 chars or longer')
+      throw new Error('Password must be 8 characters or longer.')
     }
 
     const password = await bcrypt.hash(args.data.password, 10)
-
-    return prisma.mutation.createUser({
+    const user = await prisma.mutation.createUser({
       data: {
         ...args.data,
         password
       }
-    }, info)
+    })
+
+    return {
+      user,
+      token: jwt.sign({ userId: user.id }, 'thisisasecret')
+    }
   },
   async deleteUser(parent, args, { prisma }, info) {
     return prisma.mutation.deleteUser({
