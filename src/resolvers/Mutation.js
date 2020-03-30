a3fb3d3f06ee38ca5c7d1dc5a0c66528e8c3b798
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-03-30 12:21:28 +0300

    mutations Part I

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -2,79 +2,36 @@ import uuidv4 from 'uuid/v4'
 
 const Mutation = {
   async createUser(parent, args, { prisma }, info) {
-    const emailTaken = await prisma.exists.User({ email: args.data.email })
-
-    if (emailTaken) {
-      throw new Error('Email taken')
-    }
-
     return prisma.mutation.createUser({ data: args.data }, info)
   },
   async deleteUser(parent, args, { prisma }, info) {
-    const userExists = await prisma.exists.User({ id: args.id })
-
-    if (!userExists) {
-      throw new Error('User not found')
-    }
-
     return prisma.mutation.deleteUser({
       where: {
         id: args.id
       }
     }, info)
   },
-  updateUser(parent, args, { db }, info) {
-    const { id, data } = args
-    const user = db.users.find((user) => user.id === id)
-
-    if (!user) {
-      throw new Error('User not found')
-    }
-
-    if (typeof data.email === 'string') {
-      const emailTaken = db.users.some((user) => user.email === data.email)
-
-      if (emailTaken) {
-        throw new Error('Email taken')
-      }
-
-      user.email = data.email
-    }
-
-    if (typeof data.name === 'string') {
-      user.name = data.name
-    }
-
-    if (typeof data.age !== 'undefined') {
-      user.age = data.age
-    }
-
-    return user
+  async updateUser(parent, args, { prisma }, info) {
+    return prisma.mutation.updateUser({
+      where: {
+        id: args.id
+      },
+      data: args.data
+    }, info)
   },
-  createPost(parent, args, { db, pubsub }, info) {
-    const userExists = db.users.some((user) => user.id === args.data.author)
-
-    if (!userExists) {
-      throw new Error('User not found')
-    }
-
-    const post = {
-      id: uuidv4(),
-      ...args.data
-    }
-
-    db.posts.push(post)
-
-    if (args.data.published) {
-      pubsub.publish('post', {
-        post: {
-          mutation: 'CREATED',
-          data: post
+  createPost(parent, args, { prisma }, info) {
+    return prisma.mutation.createPost({
+      data: {
+        title: args.data.title,
+        body: args.data.body,
+        published: args.data.published,
+        author: {
+          connect: {
+            id: args.data.author
+          }
         }
-      })
-    }
-
-    return post
+      }
+    }, info)
   },
   deletePost(parent, args, { db, pubsub }, info) {
     const postIndex = db.posts.findIndex((post) => post.id === args.id)
