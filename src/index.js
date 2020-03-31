cf2ea8c2914f65a3b321aaac282016f2b705666f
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-03-31 11:30:10 +0300

    adding fragments

--- src/index.js
+++ src/index.js
@@ -1,37 +1,24 @@
 import { GraphQLServer, PubSub } from 'graphql-yoga'
 import db from './db'
-import Subscription from './resolvers/Subscription'
-import Query from './resolvers/Query'
-import Mutation from './resolvers/Mutation'
-import Post from './resolvers/Post'
-import Comment from './resolvers/Comment'
-import User from './resolvers/User'
+import { resolvers, fragmentReplacements } from './resolvers/index'
 import prisma from './prisma'
-
-// Scalar types - String, Boolean, Int, Float, ID
 
 const pubsub = new PubSub()
 
 const server = new GraphQLServer({
-  typeDefs: './src/schema.graphql',
-  resolvers: {
-    Query,
-    Mutation,
-    Subscription,
-    User,
-    Post,
-    Comment
-  },
-  context(request) {
-    return {
-      db,
-      pubsub,
-      prisma,
-      request
-    }
-  }
+    typeDefs: './src/schema.graphql',
+    resolvers,
+    context(request) {
+        return {
+            db,
+            pubsub,
+            prisma,
+            request
+        }
+    },
+    fragmentReplacements
 })
 
 server.start(() => {
-  console.log('The server is up!')
+    console.log('The server is up!')
 })
