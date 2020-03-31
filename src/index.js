fc7f4f8f7fae5b1c8286b74a60f000fbb0e587c7
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-03-31 18:34:30 +0300

    NodeJs prod app deployment part I - dev server working

--- src/index.js
+++ src/index.js
@@ -6,19 +6,19 @@ import prisma from './prisma'
 const pubsub = new PubSub()
 
 const server = new GraphQLServer({
-    typeDefs: './src/schema.graphql',
-    resolvers,
-    context(request) {
-        return {
-            db,
-            pubsub,
-            prisma,
-            request
-        }
-    },
-    fragmentReplacements
+  typeDefs: './src/schema.graphql',
+  resolvers,
+  context(request) {
+    return {
+      db,
+      pubsub,
+      prisma,
+      request
+    }
+  },
+  fragmentReplacements
 })
 
-server.start(() => {
-    console.log('The server is up!')
+server.start({ port: process.env.PORT || 4000 }, () => {
+  console.log('The server is up!')
 })
